35e591e75de5e237001022aec5c1fdfb2d76d1c4
Marcos Kaio <kaionery111@gmail.com>
2024-03-25 08:51:52 -0300

    começando a criacao do layout da tela de carrinho

---
+++ js/script.js
@@ -0,0 +1,16 @@
+$(".autoplay").slick({
+  dots: true,
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  autoplay: true,
+  autoplaySpeed: 3000,
+});
+
+$(".promocoes").slick({
+  dots: true,
+  infinite: true,
+  speed: 300,
+  slidesToShow: 1,
+  centerMode: true,
+  variableWidth: true,
+});
